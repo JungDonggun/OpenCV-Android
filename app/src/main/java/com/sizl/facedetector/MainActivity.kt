c2f17affd73c827d691fe2f21c14d259af334b69
JungDonggun <donggun.dev@gmail.com>
2021-09-23 19:27:10 +0900

    Add OpenCV for Face Detection

--- app/src/main/java/com/sizl/facedetector/MainActivity.kt
+++ app/src/main/java/com/sizl/facedetector/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.sizl.facedetector
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import org.opencv.android.OpenCVLoader
 
-class bdjMainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val isInitialized = OpenCVLoader.initDebug()
+
+        Log.d("donggun","isIntialized = $isInitialized")
     }
+
+
 }
