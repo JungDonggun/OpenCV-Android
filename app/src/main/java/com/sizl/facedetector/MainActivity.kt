3bb43f47ebaf1cb3d2944f87cd21824913abaee6
JungDonggun <donggun.dev@gmail.com>
2021-09-23 16:41:20 +0900

    Initial project with kotlin

---
+++ app/src/main/java/com/sizl/facedetector/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sizl.facedetector
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class bdjMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
